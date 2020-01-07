5648241e1151a1ba88a9c9c0e7de78c809efbbf6
chenyanyin <1208165978@qq.com>
2020-01-08 02:55:08 +0800

    响应静态文件函数

--- src/response_func.h
+++ src/response_func.h
@@ -47,3 +47,31 @@ void set_response_header_content_type(int client_fd, char *header, char *value) 
     sprintf(header, "%sContent-type: %s\r\n\r\n", header, value);
     write(client_fd, header, strlen(header));
 }
+
+/*
+ * 响应静态文件(响应体设置为静态文件)
+ * @param filename 文件名
+ */
+void set_response_body_static_file(int client_fd, char *filename);
+
+void set_response_body_static_file(int client_fd, char *filename) {
+    struct stat sbuf;
+
+    // 获取文件状态
+    int file_status = stat(filename, &sbuf);
+    // 文件存在，放在外层判断，同步设置状态行
+
+
+    // 打开文件
+    int static_file_fd = open(filename, O_RDONLY);
+    char *srcp = mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, static_file_fd, 0);
+
+    // 关闭文件
+    close(static_file_fd);
+
+    // 写到到响应体
+    write(client_fd, srcp, sbuf.st_size);
+
+    // 清空空间
+    munmap(srcp, sbuf.st_size);
+}
